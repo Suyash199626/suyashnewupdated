904cc54fdea6cb1c09aaff6bfa0381cf6e9fb2f8
suyas <suyas@LAPTOP-VA9E9AHH>
2022-10-15 12:46:23 +0530

    BY-hjdgfdgf

---
+++ ggg/src/ggg/DEMO.java
@@ -0,0 +1,8 @@
+package ggg;
+
+public class DEMO 
+{
+public static void main(String[] args) {
+	System.out.println("Hello");
+}
+}
---
+++ ggg/src/module-info.java
@@ -0,0 +1,9 @@
+/**
+ * 
+ */
+/**
+ * @author suyas
+ *
+ */
+module ggg {
+}
