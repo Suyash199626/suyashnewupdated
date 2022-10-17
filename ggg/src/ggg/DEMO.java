7fbfdaa96acfdc6f6b03a4bda4ee966b1b5282ef
suyas <suyas@LAPTOP-VA9E9AHH>
2022-10-17 10:21:26 +0530

    up bracket

--- ggg/src/ggg/DEMO.java
+++ ggg/src/ggg/DEMO.java
@@ -15,3 +15,4 @@ public static void main(String[] args) {
 	public void m1(){
 		System.out.println("Hello Suyash");
 }
+}
--- ggg/src/p2/demo3.java
+++
@@ -1,9 +0,0 @@
-package p2;
-
-public class demo3
-{
-public static void main(String[] args) 
-{
-	System.out.println("hello demo 3");
-}
-}
